a8ea3e5e797c7bb74ae437091732476a2e1f3825
moclan01 <milepro98@gmail.com>
2023-11-15 14:22:00 +0700

    elephant

--- src/models/Elephant.java
+++ src/models/Elephant.java
@@ -8,39 +8,42 @@ public class Elephant extends Piece {
     public Elephant(Color color) {
         super(color);
         this.code = 4;
-        if(color.equals(Color.BLACK))
+        if (color.equals(Color.BLACK))
             this.code *= -1;
         this.strCode = "V";
     }
-    
-    private boolean isOverLake(Point point) {
-        int x = point.getX();
-        if (getColor().equals(Color.RED)) {
-            return x > 0 && x <= 4;
-        } else
-            return x >= 5 && x <= 9;
-    }
-
 
     @Override
     public List<Point> getAllPossibleMoves() {
-       List<Point> result = new ArrayList<Point>();
-       int x = this.getPoint().getX();
-       int y = this.getPoint().getY();
-       Board board = Board.getInstance();
-    
-       Point lefttop;
-       Point righttop;
-       Point leftbottom;
-       Point rightbottom;  
-    
-       if(getColor().equals(Color.RED)){
-            for(int i = x +1; i <x +3; i++){
-                lefttop = new Point(i, i)
-            }
-       }
+        List<Point> result = new ArrayList<Point>();
+        int x = this.getPoint().getX();
+        int y = this.getPoint().getY();
+        Board board = Board.getInstance();
 
-       return result;
+        Point elephantLeftTop;
+        Point elephantRightTop;
+        Point elephantLeftBottom;
+        Point elephantRightBottom;
+
+        if (getColor().equals(Color.RED)) {
+            // check left top
+            if (x - 2 >= 0 && y - 2 >= 0) {
+                elephantLeftTop = new Point(x - 2, y - 2);
+                if (board.isEmptyPosition(elephantLeftTop) || isOpponentPiece(elephantLeftTop)) {
+                    result.add(elephantLeftTop);
+                }
+            }
+            // check right top
+            if(x-2 >=0 && y -2 < board.BOARD_COLS){
+                elephantRightTop = new Point(x - 2, y + 2);
+                if(board.isEmptyPosition(elephantRightTop) || isOpponentPiece(elephantRightTop)){
+                    result.add(elephantRightTop);
+                }
+            }
+
+        }
+
+        return result;
     }
-    
+
 }
